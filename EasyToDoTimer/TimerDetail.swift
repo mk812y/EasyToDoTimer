417a7fb239c7b0fbc17f81b794a0fa7ac935e5b3
mk812y <mk812y@gmail.com>
2022-07-13 23:06:04 +0300

    add detail view timer

---
+++ EasyToDoTimer/TimerDetail.swift
@@ -0,0 +1,20 @@
+//
+//  TimerDetail.swift
+//  EasyToDoTimer
+//
+//  Created by Михаил Куприянов on 13.07.2022.
+//
+
+import SwiftUI
+
+struct TimerDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerDetail()
+    }
+}
